a270b6b531378d1087772b974bb215342aa8051a
Yessyca Garcia <yessgarciacampos@gmail.com>
2022-10-02 14:51:19 -0500

    player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    //Variables del movimiento del personaje
+    public float jumpForce = 6f;
+    public float runningSpeed = 2f;
+    
+    Rigidbody2D rigidBody;
+    Animator animator;
+
+    private const string STATE_ALIVE = "isAlive";
+    private const string STATE_ON_THE_GROUND = "isOnTheGround";
+
+    public LayerMask groundMask;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Jump")){
+            Jump();
+        }
+
+        animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
+
+
+
+        Debug.DrawRay(this.transform.position, Vector2.down * 1.5f, Color.red);
+	}
+    
+    void FixedUpdate() 
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+        if(rigidBody.velocity.x < runningSpeed){
+            rigidBody.velocity = new Vector2(runningSpeed, //eje x
+            rigidBody.velocity.y); //eje y 
+        
+        }
+        }else{//Si no estamos dentro de la partida
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y); 
+        }
+    }
+
+    void Jump()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+        if (IsTouchingTheGround())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+        }
+    }
+
+    //Nos indica si el personaje está o no tocando el suelo
+    bool IsTouchingTheGround(){
+        if(Physics2D.Raycast(this.transform.position,
+                             Vector2.down,
+                             1.5f, 
+                             groundMask)){
+           
+            
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public void Die(){
+        this.animator.SetBool(STATE_ALIVE, false);
+        GameManager.sharedInstance.GameOver();
+    }
+
+}
