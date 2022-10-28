e69531152b8ceaf681ed075a0a557af917402229
Yessyca Garcia Campos <yessgarciacampos@gmail.com>
2022-10-28 00:34:28 -0500

    reparando el collider de la roca

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,22 +7,26 @@ public class PlayerController : MonoBehaviour {
     //Variables del movimiento del personaje
     public float jumpForce = 6f;
     public float runningSpeed = 2f;
-    
+
     Rigidbody2D rigidBody;
     Animator animator;
     Vector3 startPosition;
 
-    private const string STATE_ALIVE = "isAlive";
-    private const string STATE_ON_THE_GROUND = "isOnTheGround";
+    const string STATE_ALIVE = "isAlive";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
 
+    [SerializeField]
     private int healthPoints, manaPoints;
-    public const int INITIAL_HEALTH = 100, INITIAL_MANA = 15, 
-    MAX_HEALTH = 200, MAX_MANA = 30, 
-    MIN_HEALTH = 10, MIN_MANA = 0;
-     
+
+    public const int INITIAL_HEALTH = 100, INITIAL_MANA = 15,
+        MAX_HEALTH = 200, MAX_MANA = 30,
+        MIN_HEALTH = 10, MIN_MANA = 0;
 
     public const int SUPERJUMP_COST = 5;
-    public const float SUPERJUMP_FORCE = 1.5F;
+    public const float SUPERJUMP_FORCE = 1.5f;
+
+    public float jumpRaycastDistance = 1.5f;
+
     public LayerMask groundMask;
 
     void Awake()
@@ -33,25 +37,26 @@ public class PlayerController : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-        
+
         startPosition = this.transform.position;
 	}
 
     public void StartGame(){
         animator.SetBool(STATE_ALIVE, true);
         animator.SetBool(STATE_ON_THE_GROUND, true);
-    healthPoints = INITIAL_HEALTH;
-    manaPoints = INITIAL_MANA;
 
-       Invoke("RestartPosition", 0.2f);
+        healthPoints = INITIAL_HEALTH;
+        manaPoints = INITIAL_MANA;
+
+        Invoke("RestartPosition", 0.2f);
     }
 
     void RestartPosition(){
-         this.transform.position = startPosition;
+        this.transform.position = startPosition;
         this.rigidBody.velocity = Vector2.zero;
-        // GameObject maincamera  = GameObject.Find("Main Camera");
-        // GetComponent<Camera>().GetComponent<CameraFollow>().ResetCameraPosition();
+
+        GameObject mainCamera = GameObject.Find("Main Camera");
+        mainCamera.GetComponent<CameraFollow>().ResetCameraPosition();
     }
 	
 	// Update is called once per frame
@@ -59,46 +64,47 @@ public class PlayerController : MonoBehaviour {
         if(Input.GetButtonDown("Jump")){
             Jump(false);
         }
-        if (Input.GetButtonDown("Superjump"))
-        {
+        if (Input.GetButtonDown("Superjump")){
             Jump(true);
         }
 
         animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
 
-
-
-        Debug.DrawRay(this.transform.position, Vector2.down * 1.5f, Color.red);
+        Debug.DrawRay(this.transform.position, 
+                      Vector2.down * jumpRaycastDistance, Color.red);
 	}
-    
-    void FixedUpdate() 
+
+    void FixedUpdate()
     {
         if (GameManager.sharedInstance.currentGameState == GameState.inGame)
         {
-        if(rigidBody.velocity.x < runningSpeed){
-            rigidBody.velocity = new Vector2(runningSpeed, //eje x
-            rigidBody.velocity.y); //eje y 
-        
-        }
+            if (rigidBody.velocity.x < runningSpeed)
+            {
+                rigidBody.velocity = new Vector2(runningSpeed, //x
+                                                 rigidBody.velocity.y //y
+                                                );
+            }
         }else{//Si no estamos dentro de la partida
-            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y); 
+            rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
         }
     }
 
     void Jump(bool superjump)
     {
         float jumpForceFactor = jumpForce;
-        if (superjump && manaPoints >= SUPERJUMP_COST)
-        {   
+
+        if(superjump&&manaPoints>=SUPERJUMP_COST){
             manaPoints -= SUPERJUMP_COST;
             jumpForceFactor *= SUPERJUMP_FORCE;
         }
+
         if (GameManager.sharedInstance.currentGameState == GameState.inGame)
         {
-        if (IsTouchingTheGround())
-        {
-            rigidBody.AddForce(Vector2.up * jumpForceFactor, ForceMode2D.Impulse);
-        }
+            if (IsTouchingTheGround())
+            {
+                
+                rigidBody.AddForce(Vector2.up * jumpForceFactor, ForceMode2D.Impulse);
+            }
         }
     }
 
@@ -106,40 +112,47 @@ public class PlayerController : MonoBehaviour {
     bool IsTouchingTheGround(){
         if(Physics2D.Raycast(this.transform.position,
                              Vector2.down,
-                             1.5f, 
+                             jumpRaycastDistance, 
                              groundMask)){
-           
-            
             return true;
         }else {
             return false;
         }
     }
 
+
     public void Die(){
+
         float travelledDistance = GetTravelledDistance();
-        float previousMaxDistance = PlayerPrefs.GetFloat("maxscore");
-        if (travelledDistance > previousMaxDistance)
-        {
+        float previousMaxDistance = PlayerPrefs.GetFloat("maxscore",0f);
+        if(travelledDistance > previousMaxDistance){
             PlayerPrefs.SetFloat("maxscore", travelledDistance);
         }
-
 
         this.animator.SetBool(STATE_ALIVE, false);
         GameManager.sharedInstance.GameOver();
     }
 
+
     public void CollectHealth(int points){
         this.healthPoints += points;
-        if (this.healthPoints >= MAX_HEALTH){
-            this.healthPoints = MAX_HEALTH; 
+        if(this.healthPoints >= MAX_HEALTH){
+            this.healthPoints = MAX_HEALTH;
         }
-       
+
+        if(this.healthPoints <= 0){
+            Die();
+        }
     }
 
     public void CollectMana(int points){
-
+        this.manaPoints += points;
+        if (this.manaPoints >= MAX_MANA)
+        {
+            this.manaPoints = MAX_MANA;
+        }
     }
+
     public int GetHealth(){
         return healthPoints;
     }
@@ -150,5 +163,6 @@ public class PlayerController : MonoBehaviour {
 
     public float GetTravelledDistance(){
         return this.transform.position.x - startPosition.x;
-    } 
+    }
+
 }
