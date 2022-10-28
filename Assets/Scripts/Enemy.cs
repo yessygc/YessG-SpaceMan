017f4a7feb6b6f271f539c3fb2960d76dc5d73db
Yessyca Garcia Campos <yessgarciacampos@gmail.com>
2022-10-28 00:31:55 -0500

    preparando al enemigo para girar

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,30 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
+public class Enemy : MonoBehaviour {
 
     public float runningSpeed = 1.5f;
 
-    Rigidbody2D rigidBody2D;
+    public int enemyDamage = 10;
 
-    public bool facingright = false;
+    Rigidbody2D rigidBody;
+
+    public bool facingRight = false;
 
     private Vector3 startPosition;
 
-    private void Awake() {
-        rigidBody2D = GetComponent<Rigidbody2D>();
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
         startPosition = this.transform.position;
     }
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    // Use this for initialization
+    void Start () {
         this.transform.position = startPosition;
+	}
+
+    private void FixedUpdate()
+    {
+        float currentRunningSpeed = runningSpeed;
+
+        if(facingRight){
+            //Mirando hacia la derecha
+            currentRunningSpeed = runningSpeed;
+            this.transform.eulerAngles = new Vector3(0, 180, 0);
+        } else{
+            //Mirando hacia la izquierda
+            currentRunningSpeed = -runningSpeed;
+            this.transform.eulerAngles = Vector3.zero;
+        }
+
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame){
+            rigidBody.velocity = new Vector2(currentRunningSpeed, 
+                                             rigidBody.velocity.y);
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.tag == "Coin"){
+            return;
+        }
+
+        if(collision.tag == "Player"){
+            collision.gameObject.GetComponent<PlayerController>().
+                     CollectHealth(-enemyDamage);
+            return;
+        }
+
+        //Si llegamos aquí, no hemos chocado ni con monedas, ni con players
+        //Lo más normal es que aquí haya otro enemigo o bien escenario
+        //Vamos a hacer que el enemigo rote
+        facingRight = !facingRight;
     }
+
+
 }
